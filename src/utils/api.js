313f1288685c2b316312627b67e4bdd8dc127d27
ivanna-balanyuk <ivannabalanyuk@yandex.ru>
2023-08-29 12:29:22 +0300

    replace and fix baseUrl constant

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
-import { baseUrl } from "../utils/constants";
+const baseUrl = 'https://norma.nomoreparties.space/api';
 
 function checkResult(res) {
   return res.ok ? res.json() : Promise.reject(res.status);
 }
 
 function getIngredientsData() {
-  return fetch(baseUrl, {
+  return fetch(`${baseUrl}/ingredients`, {
     method: 'GET'
   })
   .then((res) => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
 export const modalRoot = document.getElementById("modal-root");
-export const baseUrl = 'https://norma.nomoreparties.space/api/ingredients';
 export const doneImage = '../images/order-accpeted-popup-done.png';
