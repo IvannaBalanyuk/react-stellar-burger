a61bd423b0d93f4b1a422049f8fa7e25793a97bf
ivanna-balanyuk <ivannabalanyuk@yandex.ru>
2023-09-15 18:10:59 +0300

    внесены правки по оформлению

--- src/components/burger-ingredients/components/tabs-panel/tabs-panel.jsx
+++ src/components/burger-ingredients/components/tabs-panel/tabs-panel.jsx
@@ -3,54 +3,60 @@ import styles from "./tabs-panel.module.css";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
 
-const TabsPanel = React.memo(({ bunRef, sauceRef, mainRef, current, setCurrent }) => {
-
-  return (
-    <div className={`${styles.wrapper} mb-10`}>
-      <Tab
-        value="bun"
-        active={current === "bun"}
-        onClick={() => {
-          setCurrent("bun");
-          bunRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
-        }}
-      >
-        Булки
-      </Tab>
-      <Tab
-        value="sauce"
-        active={current === "sauce"}
-        onClick={() => {
-          setCurrent("sauce");
-          sauceRef.current.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-          });
-        }}
-      >
-        Соусы
-      </Tab>
-      <Tab
-        value="main"
-        active={current === "main"}
-        onClick={() => {
-          setCurrent("main");
-          mainRef.current.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-          });
-        }}
-      >
-        Начинки
-      </Tab>
-    </div>
-  );
-});
+const TabsPanel = React.memo(
+  ({ bunRef, sauceRef, mainRef, current, setCurrent }) => {
+    return (
+      <div className={`${styles.wrapper} mb-10`}>
+        <Tab
+          value="bun"
+          active={current === "bun"}
+          onClick={() => {
+            setCurrent("bun");
+            bunRef.current.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+          }}
+        >
+          Булки
+        </Tab>
+        <Tab
+          value="sauce"
+          active={current === "sauce"}
+          onClick={() => {
+            setCurrent("sauce");
+            sauceRef.current.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+          }}
+        >
+          Соусы
+        </Tab>
+        <Tab
+          value="main"
+          active={current === "main"}
+          onClick={() => {
+            setCurrent("main");
+            mainRef.current.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+          }}
+        >
+          Начинки
+        </Tab>
+      </div>
+    );
+  }
+);
 
 TabsPanel.propTypes = {
   bunRef: PropTypes.object.isRequired,
   sauceRef: PropTypes.object.isRequired,
   mainRef: PropTypes.object.isRequired,
+  current: PropTypes.object.isRequired,
+  setCurrent: PropTypes.func.isRequired,
 };
 
 export default TabsPanel;
